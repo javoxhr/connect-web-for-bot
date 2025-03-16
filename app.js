0d7e99ff7b83c7eab7a053cf5186f999d953ee74
javoxhr <javohiruzum@gmail.com>
2025-03-16 16:06:23 +0500

    new

--- app.js
+++ app.js
@@ -7,19 +7,18 @@ const main = document.querySelector("#main")
 const mainForm = document.querySelector("#main-form")
 const pushDataBtn = document.querySelector("#push-data-btn")
 
-pushDataBtn.addEventListener("click", ()=> {
+pushDataBtn.addEventListener("click", () => {
     mainForm.classList.add("active-main-form")
     main.classList.add("off-main")
 })
 
 const form = document.querySelector("#form")
-const getName = document.querySelector("#name").value
-const getUserName = document.querySelector("#username").value
-const getPassword = document.querySelector("#password").value
 
-form.addEventListener("submit", (e)=> {
+form.addEventListener("submit", (e) => {
     e.preventDefault()
-
+    const getName = document.querySelector("#name").value
+    const getUserName = document.querySelector("#username").value
+    const getPassword = document.querySelector("#password").value
     let data = {
         name: getName,
         username: getUserName,
