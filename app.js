88837d9e7d2fba8b55c7c75da7995810278b878b
javoxhr <javohiruzum@gmail.com>
2025-03-16 15:28:14 +0500

    new

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+let tg = window.Telegram.WebApp
+
+
 const main = document.querySelector("#main")
 const mainForm = document.querySelector("#main-form")
 const pushDataBtn = document.querySelector("#push-data-btn")
