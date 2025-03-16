d45129a8a013040cb843e67483ec4267883f307e
javoxhr <javohiruzum@gmail.com>
2025-03-16 14:49:49 +0500

    new

---
+++ app.js
@@ -0,0 +1,8 @@
+const main = document.querySelector("#main")
+const mainForm = document.querySelector("#main-form")
+const pushDataBtn = document.querySelector("#push-data-btn")
+
+pushDataBtn.addEventListener("click", ()=> {
+    mainForm.classList.add("active-main-form")
+    main.classList.add("off-main")
+})
